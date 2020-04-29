21316d189a1eb2088d49a64570257f0c0bb061eb
LeraShur <valeriia.shurupina@yahoo.com>
2020-04-29 12:45:16 +0200

    Updates

---
+++ precache-manifest.2a6b45fb8f491c94f8f982b278406a75.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74c8a006e509f9704714a4dec754bea8",
+    "url": "/spb/public/index.html/index.html"
+  },
+  {
+    "revision": "ec08e99ac820bcab0652",
+    "url": "/spb/public/index.html/static/css/2.7ad897a5.chunk.css"
+  },
+  {
+    "revision": "2568360ceb7c799bc288",
+    "url": "/spb/public/index.html/static/css/main.bbe55019.chunk.css"
+  },
+  {
+    "revision": "ec08e99ac820bcab0652",
+    "url": "/spb/public/index.html/static/js/2.3dda530a.chunk.js"
+  },
+  {
+    "revision": "15a97cb02185e6bf23872356a364b3ba",
+    "url": "/spb/public/index.html/static/js/2.3dda530a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2568360ceb7c799bc288",
+    "url": "/spb/public/index.html/static/js/main.a28a0758.chunk.js"
+  },
+  {
+    "revision": "7b938d3f3b9920c5e9f4",
+    "url": "/spb/public/index.html/static/js/runtime-main.ffa4ff45.js"
+  }
+]);
